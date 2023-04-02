a310860b6c57a1b332ee4c2dce04de75cde356e8
Paweł Śniadała <biner.home@gmail.com>
2023-04-02 15:01:15 +0200

    Creating a new project based on the next framework

---
+++ app/src/app/memes/page.tsx
@@ -0,0 +1,4 @@
+// `app/page.js` is the UI for the root `/` URL
+export default function Memes() {
+    return <h1>Memes</h1>;
+}
---
+++ app/src/app/services.tsx
@@ -0,0 +1,4 @@
+// `app/page.js` is the UI for the root `/` URL
+export default function Services() {
+    return <h1>Services</h1>;
+}
